07c22f9255c3c2e1f2a3c920d2c5232abd9e8ee7
alexasantos <alexandra.iv.santos@gmail.com>
2016-02-20 22:47:53 +0000

    fixed random issues and shit

--- app/main.py
+++ app/main.py
@@ -12,14 +12,14 @@ def static(path):
 @bottle.get('/')
 
 def index():
-    head_url = 'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiNrYS9rIfLAhUBz2MKHdhZA18QjRwIBw&url=http%3A%2F%2Ftheultralinx.com%2Ftag%2Fgif%2F&psig=AFQjCNE-U5WU-jW1hh5iKxSp_aT6bp4uPQ&ust=1456092250871705' % (
+    head_url = '%s://%s/static/head.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
 
     return {
         'color': '#8E4585',
-        'head': "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fstorage.canoe.ca%2Fv1%2Fdynamic_resize%2Fsws_path%2Fsuns-prod-images%2F1297411412978_ORIGINAL.jpg%3Fquality%3D80%26size%3D650x&f=1"
+        'head': head_url
     }
 
 
