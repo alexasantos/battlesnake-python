9f69ce17d3a8947511fa24bd552de691ec6e9341
alexasantos <alexandra.iv.santos@gmail.com>
2016-02-21 01:13:34 +0000

    fixed random issues and shit

--- app/main.py
+++ app/main.py
@@ -11,13 +11,13 @@ def static(path):
 @bottle.get('/')
 
 def index():
-    head_url = '%s://%s/static/head.png' % (
+    head_url = '%s://%s/static/snake.jpg' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
 
     return {
-        'color': '#8E4585',
+        'color': '#E0218A',
         'head': head_url
     }
 
